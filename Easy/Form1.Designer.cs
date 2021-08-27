f67f6699671048e1b41b1b2e4151e2480daab4e1
ELLIANCE I7 <ELLIANCE I7@DESKTOP-GL310SE>
2021-08-27 10:47:38 +0800

    Add project files.

---
+++ Easy/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Easy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.T3 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(403, 134);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.editToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(150, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 22);
+            this.toolStripMenuItem1.Text = "Send Message";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.editToolStripMenuItem.Text = "Edit";
+            this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 153);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(487, 225);
+            this.listBox1.TabIndex = 2;
+            // 
+            // T3
+            // 
+            this.T3.Interval = 1000;
+            this.T3.Tick += new System.EventHandler(this.T3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 396);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.Timer T3;
+    }
+}
+
---
+++ Easy/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Easy
+{
+    public partial class Form1 : Form
+    {
+        static IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+        static int PortNumber = 5001;
+        TcpListener ServerListener = new TcpListener(ipAd, PortNumber);
+        TcpClient clientSocket = default(TcpClient);
+        delegate void SetTextCallback(string text);
+        NetworkStream networkStream = null;
+        byte[] first;
+        byte[] second;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread thread2 = new Thread(StartServer);
+            thread2.Start();
+
+            treeView1.Nodes.Add("S1F1 - Are you there");
+            treeView1.Nodes[0].Nodes.Add("R - Are you there request");
+            treeView1.Nodes[0].Nodes.Add("D - On line data");
+            treeView1.Nodes[0].Nodes[1].Nodes.Add("L");
+            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add("A");
+            treeView1.Nodes[0].Nodes[1].Nodes[0].Nodes.Add("A");
+
+            treeView1.Nodes.Add("S1F3 - Selected equipment status");
+            treeView1.Nodes[1].Nodes.Add("SSR - Selected equipment status request");
+            treeView1.Nodes[1].Nodes[0].Nodes.Add("L");
+            treeView1.Nodes[1].Nodes[0].Nodes[0].Nodes.Add("U4");
+            treeView1.Nodes[1].Nodes.Add("SSD - Selected equipment status data");
+            treeView1.Nodes[1].Nodes[1].Nodes.Add("L");
+            treeView1.Nodes[1].Nodes[1].Nodes[0].Nodes.Add("V");
+
+            treeView1.Nodes.Add("S1F5 - Formatted status");
+            treeView1.Nodes[2].Nodes.Add("FSR - Formatted status request");
+            treeView1.Nodes[2].Nodes[0].Nodes.Add("B");
+            treeView1.Nodes[2].Nodes.Add("FSD - Formatted status data");
+        }
+
+        private void StartServer()
+        {
+            ServerListener.Start();
+            clientSocket = ServerListener.AcceptTcpClient();
+            SetText("Connected to Client");
+
+            while (true)
+            {
+                try
+                {
+                    networkStream = clientSocket.GetStream();
+                    byte[] readBytes = new byte[clientSocket.ReceiveBufferSize];
+                    int bytesread = networkStream.Read(readBytes, 0, clientSocket.ReceiveBufferSize);
+
+                    byte[] bytesreceived = readBytes.Take(bytesread).ToArray();
+
+                    if (bytesreceived.Length == 14)
+                    {
+                        if (bytesreceived[9] == 5)
+                        {
+                            SetText("Linktest Request  \n  " + BitConverter.ToString(bytesreceived));
+                            processlinktest(bytesreceived);
+                        }
+                        else if (bytesreceived[9] == 1)
+                        {
+                            SetText("Select Request  \n  " + BitConverter.ToString(bytesreceived));
+                            processselect(bytesreceived);
+                        }
+                    }
+
+                    else if (bytesreceived.Length > 4)
+                    {
+                        second = bytesreceived;
+                        SetText("S1F2: " + BitConverter.ToString(bytesreceived));
+                        //Combine(first, second);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    SetText(ex.Message);
+                }
+            }
+        }
+
+        private void processlinktest(byte[] bytesreceived)
+        {
+            if (bytesreceived.Length > 10)
+            {
+                byte[] sendBytes = new byte[] { 0, 0, 0, 10, 255, 255, 0, 0, 0, 6, 0, 0, 0, 2 };
+                networkStream.Write(sendBytes, 0, sendBytes.Length);
+                SetText("Linktest Response " + BitConverter.ToString(sendBytes));
+            }
+        }
+
+        private void processselect(byte[] bytesreceived)
+        {
+            if (bytesreceived.Length > 10)
+            {
+                byte[] sendBytes = new byte[] { 0, 0, 0, 10, 255, 255, 0, 0, 0, 2, 0, 0, 0, 1 };
+                networkStream.Write(sendBytes, 0, sendBytes.Length);
+                SetText("Select Response \n" + BitConverter.ToString(sendBytes));
+            }
+        }
+
+        public void Combine(byte[] first, byte[] second)
+        {
+            byte[] ret = new byte[first.Length + second.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            processsecs(ret);
+        }
+
+        private void processsecs(byte[] ret)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SetText(string text)
+        {
+            if (this.listBox1.InvokeRequired)
+            {
+
+                listBox1.Invoke(new SetTextCallback(SetText), text);
+            }
+
+            else
+            {
+                this.listBox1.Items.Add(text);
+            }
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                treeView1.SelectedNode = treeView1.GetNodeAt(e.X, e.Y);
+
+                if (treeView1.SelectedNode != null)
+                {
+                    contextMenuStrip1.Show(treeView1, e.Location);
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {   
+            try
+            {
+                byte[] sends1f1 = new byte[] { 00, 00, 00, 10, 00, 01, 129, 01, 00, 00, 00, 00, 00, 01 };
+                networkStream.Write(sends1f1, 0, sends1f1.Length);
+                T3.Start();
+                SetText("S1F1: " + BitConverter.ToString(sends1f1));
+            }
+            catch (Exception ex)
+            {
+                SetText(ex.Message);
+            }
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void T3_Tick(object sender, EventArgs e)
+        {
+            int x = 0;
+            x++;
+            if (x == 5)
+            {
+                SetText("no reply");
+            }
+        }
+    }
+}
